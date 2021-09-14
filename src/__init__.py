4ec57d7682a9892f360bd577efe401d8bdff5a38
Ranshi <782938005@qq.com>
2021-09-14 21:12:07 +0800

    修改了目录结构, 将__init__.py修改为更具有阅读性的名称

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding:utf-8 -*-
-# @Time     : 2021/08/04 21:31
-# @Author   : Ranshi
-# @File     : __init__.py
-# @Doc      : 源代码包
--- src/api/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding:utf-8 -*-
-# @Time     : 2021/08/04 15:33
-# @Author   : Ranshi
-# @File     : __init__.py
-# @Doc      : 文件夹存放整个项目的公用api
